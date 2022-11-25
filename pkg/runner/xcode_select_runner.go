cbdc9388c56ded52d21f3aa996248f2a2b7d8586
gzzhangkai2014 <gzzhangkai2014@corp.netease.com>
2022-11-26 02:24:24 +0800

    Set DEVELOPER_DIR and SDKROOT accoording to env

--- pkg/runner/xcode_select_runner.go
+++ pkg/runner/xcode_select_runner.go
@@ -3,6 +3,8 @@ package runner
 import (
 	"context"
 	"fmt"
+	"os"
+	"strings"
 
 	runner_pb "github.com/buildbarn/bb-remote-execution/pkg/proto/runner"
 
@@ -26,8 +28,23 @@ func (r *xcodeSelectRunner) CheckReadiness(ctx context.Context, request *emptypb
 }
 
 func (r *xcodeSelectRunner) Run(ctx context.Context, request *runner_pb.RunRequest) (*runner_pb.RunResponse, error) {
-	for name, value := range request.EnvironmentVariables {
-		fmt.Print(name+"="+value)
+	if xcodeVersionOverride, ok := request.EnvironmentVariables["XCODE_VERSION_OVERRIDE"]; ok {
+		buildVIndex := strings.LastIndex(xcodeVersionOverride, ".")
+		xcodeVersionShort := xcodeVersionOverride[:buildVIndex]
+		developerDir := "/Applications/Xcode-"+xcodeVersionShort+".app/Contents/Developer/"
+		if _, err := os.Stat(developerDir); !os.IsNotExist(err) {
+			request.EnvironmentVariables["DEVELOPER_DIR"] = developerDir
+			if appleSdkPlatform, ok := request.EnvironmentVariables["APPLE_SDK_PLATFORM"]; ok {
+				appleSdkVersionOverride, found := request.EnvironmentVariables["APPLE_SDK_VERSION_OVERRIDE"]
+				if !found {
+					appleSdkVersionOverride = ""
+				}
+				sdkRoot := fmt.Sprintf("%sPlatforms/%s.platform/Developer/SDKs/%s%s.sdk", developerDir, appleSdkPlatform, appleSdkPlatform, appleSdkVersionOverride)
+				if _, err := os.Stat(sdkRoot); !os.IsNotExist(err) {
+					request.EnvironmentVariables["SDKROOT"] = sdkRoot
+				}
+			}
+		}
 	}
 	response, err := r.base.Run(ctx, request)
 	if err != nil {
